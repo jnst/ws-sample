517c809d5e192a7cfadcb44ed925f4b084c8e4bb
jnst <jnst@outlook.com>
2018-08-08 13:48:58 +0900

    Support broadcast

--- src/server.ts
+++ src/server.ts
@@ -4,15 +4,28 @@ import * as WebSocket from 'ws';
 import { AddressInfo } from 'net';
 
 const app = express();
-
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
+
+const REGEX_BROADCAST = /^broadcast\:/;
 
 wss.on('connection', (ws: WebSocket) => {
 
   ws.on('message', (message: string) => {
     console.log('received: %s', message);
-    ws.send(`Hello, you sent -> ${message}`);
+
+    if (REGEX_BROADCAST.test(message)) {
+      message = message.replace(REGEX_BROADCAST, '');
+      wss.clients.forEach(client => {
+        if (client != ws) {
+          client.send(`Hello, broad cast message -> ${message}`);
+        } else {
+          console.log(`self message: ${message}`);
+        }
+      });
+    } else {
+      ws.send(`Hello, you sent -> ${message}`);
+    }
   });
 
   ws.send('Hi there, I am a WebSocket server');
@@ -20,6 +33,6 @@ wss.on('connection', (ws: WebSocket) => {
 });
 
 server.listen(process.env.PORT || 8999, () => {
-  const {port} = server.address() as AddressInfo;
+  const { port } = server.address() as AddressInfo;
   console.log(`Server started on port ${port} :)`);
 });
